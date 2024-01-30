0f6fd4c07358089e32266637c4d4939964417ca1
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-30 21:18:31 +0530

    Update bot.go

--- bot.go
+++ bot.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("1711796263:AAGwSl_kQXts-Q4Q_5NjuWcgUcncinO8M7M")
+	bot, err := tgbotapi.NewBotAPI("YOUR_BOT_TOKEN")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,6 +30,8 @@ func main() {
 		bio := getUserBio(user)
 
 		if bio != "" && hasLink(bio) {
+			log.Printf("Banning user %s with link in bio", user.UserName)
+
 			kickConfig := tgbotapi.KickChatMemberConfig{
 				ChatMemberConfig: tgbotapi.ChatMemberConfig{
 					ChatID: chatID,
@@ -41,6 +43,8 @@ func main() {
 			_, err := bot.KickChatMember(kickConfig)
 			if err != nil {
 				log.Println("Failed to ban user:", err)
+			} else {
+				log.Printf("User %s banned successfully", user.UserName)
 			}
 		}
 	}
@@ -50,6 +54,7 @@ func getUserBio(user *tgbotapi.User) string {
 	// Implement a function to get user bio information
 	// based on available fields or methods in the tgbotapi.User type.
 	// Return an empty string if bio information is not available.
+	// Example: return user.Bio
 	return ""
 }
 
